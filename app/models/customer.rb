b5ab9194ca84ed52729b953bffe87479e4e68b0e
deepujohn <deepujohn.ideology@gmail.com>
2014-07-09 17:26:18 +0530

    Before applying bootstrap theme to controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ActiveRecord::Base
   belongs_to :city
+  validates :name,:email,:city,:country,:phone_number,presence:true 
+  validates :name,format: {with: /\A[a-zA-Z ]+\z/,message: 'Only letters'}
+  validates :email,format: {with:   /[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}/,message: "Enter a valid email"}
+  validates :phone_number,format: {with: /[0-9]/,message: 'Enter your phone number'}
+  validates :phone_number,length:{minimum:10,maximum:13,too_short:"Enter your phone number",too_long:"Enter your phone number"}
 end
